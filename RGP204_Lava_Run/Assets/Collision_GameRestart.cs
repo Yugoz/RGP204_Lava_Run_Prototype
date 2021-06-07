1e47536afbf370267699e374f6093c29ca234a3d
Yugoz <ugo.moretto@icloud.com>
2021-06-07 17:30:26 +1000

    Added player, camera follow, mouse cursor movement and game restart on collision

---
+++ RGP204_Lava_Run/Assets/Collision_GameRestart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collision_GameRestart : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
