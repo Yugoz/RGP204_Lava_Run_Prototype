24af7536763a5148faf1ac6fff73266d6477a634
Liam Burnett-Blue <liam@burnettblue.com>
2021-06-08 14:54:12 +1000

    Added Bolder and made it roll

---
+++ RGP204_Lava_Run/Assets/Scripts/MoveBolder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBolder : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    private float horizontalInput;
+    private float verticalInput;
+    private float speed = 10.0f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.AddForce(Vector3.right);
+        rb.AddForce(new Vector3(horizontalInput,0.0f,verticalInput) * speed);
+
+    }
+}
