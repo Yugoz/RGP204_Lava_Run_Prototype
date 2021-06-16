d3075ec66c6cfb0eb403cae1a13642ff445240b4
Yugoz <ugo.moretto@icloud.com>
2021-06-16 21:59:11 +1000

    pretty much done

---
+++ RGP204_Lava_Run/Assets/Scripts/Lava_Collision_Boost.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava_Collision_Boost : MonoBehaviour
+{
+    
+}
---
+++ RGP204_Lava_Run/Assets/Scripts/Lava_Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava_Movement : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    void Update()
+    {
+
+        transform.position += transform.up * speed * Time.deltaTime;
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Boost"))
+        {
+            speed += 0.5f;
+        }
+
+    }
+}
